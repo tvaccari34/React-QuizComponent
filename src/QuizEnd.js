d4ecfc672197f975e53dc10c57b8f89a2212a034
Tiago Vaccari <tiago.vaccari@outlook.com>
2019-02-20 17:12:48 +0000

    Completed the module

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -5,7 +5,7 @@ class QuizEnd extends Component {
         return (
             <div>
                 <p>Thanks for playing!</p>
-                <a href='#'>Reset Quiz</a>
+                <a href=''>Reset Quiz</a>
             </div>
         )
     }
